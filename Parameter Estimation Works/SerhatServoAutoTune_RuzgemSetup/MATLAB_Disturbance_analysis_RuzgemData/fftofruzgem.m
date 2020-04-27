3687db6d9416ad3f5f6b1b566b177dfc5fc5aa29
SerhatOzkucuk <52490208+SerhatOzkucuk@users.noreply.github.com>
2020-04-27 15:33:46 +0300

    update FFT analysis slx and m
    Pure data are transformed in freq domain. Some comments about them are done.

--- Parameter Estimation Works/SerhatServoAutoTune_RuzgemSetup/MATLAB_Disturbance_analysis_RuzgemData/fftofruzgem.m
+++ Parameter Estimation Works/SerhatServoAutoTune_RuzgemSetup/MATLAB_Disturbance_analysis_RuzgemData/fftofruzgem.m
@@ -1,18 +1,30 @@
 %%
 x=fft(ScopeData.signals(1).values);
 plot(abs(x));
+box on;
+grid on;
 %%
 x=fft(ScopeData.signals(2).values);
 plot(abs(x));
+box on;
+grid on;
 %%
 x=fft(ScopeData.signals(3).values);
 plot(abs(x));
+box on;
+grid on;
 %%
 x=fft(ScopeData.signals(4).values);
 plot(abs(x));
+box on;
+grid on;
 %%
 x=fft(ScopeData.signals(5).values);
 plot(abs(x));
+box on;
+grid on;
 %%
 x=fft(ScopeData.signals(6).values);
 plot(abs(x));
+box on;
+grid on;
