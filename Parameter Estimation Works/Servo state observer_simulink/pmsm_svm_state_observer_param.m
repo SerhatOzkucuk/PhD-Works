42e025d6d4a1cff53e1618ae18f32cd425ca8b38
SerhatOzkucuk <52490208+SerhatOzkucuk@users.noreply.github.com>
2020-09-21 13:23:26 +0300

    PMSM state observer simulink model
    mathematical model of the PMSM is added to ready to use model.

---
+++ Parameter Estimation Works/Servo state observer_simulink/pmsm_svm_state_observer_param.m
@@ -0,0 +1,22 @@
+%% pmsm_svm_state_observer
+%ready_to_use_model_parameters
+Rs=0.61;
+Ld=9.1e-3;
+Lq=11.5e-3;
+phi_magnet=0.175;
+J=0.0027;
+B=0.0004924;
+pp=4;
+Tf=0;
+%mathematical_model_parameters
+Rs_1=0.61;
+Ld_1=9.1e-3;
+Lq_1=11.5e-3;
+phi_magnet_1=0.175;
+pp_1=4;
+J_1=0.0027;
+B_1=0.0004924;
+
+
+
+
