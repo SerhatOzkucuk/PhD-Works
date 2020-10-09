110ee3906519131c93aad7ef700e026586ccd5a1
SerhatOzkucuk <52490208+SerhatOzkucuk@users.noreply.github.com>
2020-10-09 16:23:46 +0300

    Gear fault model and math operations
    Fault definition, mathematical model, improved model report and matlab works

---
+++ Parameter Estimation Works/Servo driveline transmission and fault math model_matlab/fault_math.m
@@ -0,0 +1,34 @@
+%% Servo driveline transmission and fault math model
+t=0:1:360;       %time
+t_0=100;            %time of fault
+tetha_m=0:1:360;    %motor gear position angle
+r_m=2;              %motor radius
+tetha_l=0:1/5:360/5;     %load gear position angle
+r_l=10;              %load radius   
+
+subplot(2,1,1);
+plot(tetha_m);
+
+subplot(2,1,2);
+plot(tetha_l);
+
+%tetha_m*r_m=tetha_l*r_l
+
+%%
+
+%fault model functions u(t)+beta*dirac(t)
+beta=1;             %fault indicator
+u=heaviside(t);
+d=(t==t_0);
+
+y=u+(beta*d);
+
+plot(t,y);
+
+%%
+
+out=y.*tetha_m*r_m;  %fault on the motor gear position
+
+plot(out);
+
+
